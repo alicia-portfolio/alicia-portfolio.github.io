288e35035da339d8b8dfdabea42d9f31d7a69086
yingyangle <cyang1999@gmail.com>
2022-02-15 02:06:35 +0100

    basic layout

--- script.js
+++ script.js
@@ -13,3 +13,34 @@ onReady(function() {
 	$('#overlay').css('opacity', 0)
 		.css('visibility', 'hidden')
 })
+
+// animated scroll to selected section
+$('#navbar a').on('click', function() {
+	console.log($(this).attr('href'))
+	$('html, body').animate({
+		scrollTop: $($(this).attr('href')).offset().top
+	}, 1000)
+})
+
+// // navbar collapse
+// $("#hamburger").on("click", () => {
+// 	$("#hamburger").toggleClass("collapsed");
+// 	$("#navbar-items").toggleClass("collapsed");
+// });
+// $(document).click(function (event) {
+// 	var clickover = $(event.target);
+// 	var opened = !$("#navbar-items").hasClass("collapsed");
+// 	if (
+// 		opened == true &&
+// 		!clickover.hasClass("navbar-toggler") &&
+// 		clickover.parents(".navbar").length == 0
+// 	) {
+// 		$("#hamburger").addClass("collapsed");
+// 		$("#navbar-items").addClass("collapsed");
+// 	}
+// });
+
+// load all artwork images for a given section
+function loadImages(section) {
+	return
+}
